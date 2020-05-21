72013d9e3acb5e8c33e264c4a176cdcf3993ae9f
MikeChunko <chunkomike@gmail.com>
2020-05-21 19:12:49 -0400

    Snake can't go back on itself

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ pyg.display.update()
 pyg.display.set_caption("Boa")
 clock = pyg.time.Clock()
 
-red, green, blue = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+red, green, blue = (200, 0, 0), (0, 200, 0), (0, 0, 200)
 size_x = size_y = 10
-delta_x, delta_y = 0, -size_y
+d_x, d_y = 0, -size_y
 
 # List containing all segments of the snake
 snake = [(250, 250), (250, 260), (250, 270)]
@@ -23,19 +23,20 @@ while running:
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             running = False
+        # Movement keys
         if event.type == pyg.KEYDOWN:
-            if event.key == pyg.K_LEFT or event.key == pyg.K_a:
-                delta_x, delta_y = -size_x, 0
-            elif event.key == pyg.K_RIGHT or event.key == pyg.K_d:
-                delta_x, delta_y = size_x, 0
-            elif event.key == pyg.K_UP or event.key == pyg.K_w:
-                delta_x, delta_y = 0, -size_y
-            elif event.key == pyg.K_DOWN or event.key == pyg.K_s:
-                delta_x, delta_y = 0, size_y
+            if (event.key == pyg.K_LEFT or event.key == pyg.K_a) and d_x <= 0:
+                d_x, d_y = -size_x, 0
+            elif (event.key == pyg.K_RIGHT or event.key == pyg.K_d) and d_x >= 0:
+                d_x, d_y = size_x, 0
+            elif (event.key == pyg.K_UP or event.key == pyg.K_w) and d_y <= 0:
+                d_x, d_y = 0, -size_y
+            elif (event.key == pyg.K_DOWN or event.key == pyg.K_s) and d_y >= 0:
+                d_x, d_y = 0, size_y
 
     # Add new segment, remove old
     x, y = snake[len(snake) - 1]
-    snake.append((x + delta_x, y + delta_y))
+    snake.append((x + d_x, y + d_y))
     snake.pop(0)
 
     # Draw
@@ -44,7 +45,7 @@ while running:
         pyg.draw.rect(screen, green, [x, y, size_x, size_y])
 
     pyg.display.update()
-    clock.tick(30)
+    clock.tick(20)
 
 pyg.quit()
 quit()
